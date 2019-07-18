19b89da841430182b3d9e2b28369615934420633
Michal Fikejs <michal@fikejs.com>
2019-07-18 10:39:33 +0200

    We will use different lib and added ttl

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-var Cache = require('async-disk-cache');
+var Cache = require('file-system-cache');
+
+const ttl = process.env.CACHE_PAGE_TTL || 86400000;
 
 module.exports = {
 	init: function() {
-		this.cache = new Cache('prerender', {
-      compression: 'gzip',
-    });
+		this.cache = Cache.default();
 	},
 
 	requestReceived: function(req, res, next) {
-		this.cache.get(req.prerender.url, function (result) {
-			if (result.isCached) {
+		this.cache.get(req.prerender.url).then(function (result) {
+			if (result && Date.now() - result.time < ttl) {
 				req.prerender.cacheHit = true;
-				res.send(200, result.value);
+				res.send(200, result.content);
 			} else {
 				next();
 			}
-		});
+		}).catch((error) => console.error(error));
 	},
 
 	beforeSend: function(req, res, next) {
 		if (!req.prerender.cacheHit && req.prerender.statusCode == 200) {
-			this.cache.set(req.prerender.url, req.prerender.content);
+			this.cache.set(req.prerender.url, {
+                time: Date.now(),
+                content: req.prerender.content,
+            });
 		}
 		next();
 	}
